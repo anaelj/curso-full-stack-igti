e96cd113220d0a764a090aa69740ec0fec20ba32
Anael <anaelj@gmail.com>
2021-05-01 22:14:34 -0400

    front end

--- _desafio-final-apoio/app/client/src/App.js
+++ _desafio-final-apoio/app/client/src/App.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Button, Card, Row, Col, Icon } from 'react-materialize';
 
 export default function App() {
-  return <h1>Desafio Final do Bootcamp Full Stack</h1>;
+  return (
+  <>
+    <h1>Desafio Final do Bootcamp Full Stack</h1>
+    <h3>Controle Financeiro Pessoal</h3>
+    <Card>
+      <Row>
+        <Col>
+          <li> Texto
+          </li>
+        </Col>
+        <Col>
+          <Button style={{ margin: '5px'}}><Icon>edit</Icon></Button>
+          <Button style={{ margin: '5px'}}><Icon>delete</Icon></Button>
+        </Col>
+      </Row>      
+      
+    </Card>
+  </>
+  )
+
+  ;
 }
